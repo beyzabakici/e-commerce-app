46c0666ab1ba5ab7045a9f7cdb4fb02e79fccd5a
Melike Beyza BAKICI <46328844+beyzabakici@users.noreply.github.com>
2021-06-06 00:06:05 +0300

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import api from './api';
 import ProductCard from './components/ProductCard/ProductCard';
 import Navbar from './components/Navbar/Navbar';
+import { Route } from 'react-router';
+import Favories from './components/Favories/Favories';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
@@ -17,11 +20,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className='card-container'>
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product} />
-        })}
-      </div>
+      <Route exact path="/">
+        <div className='card-container'>
+          {products.map((product) => {
+            return <ProductCard key={product.id} product={product} />
+          })}
+        </div>
+      </Route>
+      <Route path="/favories" component={Favories} />
+      <Route path="/cart" component={Cart} />
     </div>
   );
 }
